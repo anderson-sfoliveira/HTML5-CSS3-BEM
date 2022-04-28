dadbd1f4f056ee16bf9451ffcd5f384856c8c5f2
Anderson Oliveira <2175235+anderson-sfoliveira@users.noreply.github.com>
2022-04-28 14:09:24 -0300

    ajustes nos links

---
+++ javascripts/validacao.js
@@ -0,0 +1,6 @@
+function logSubmit(event) {
+    alert("Projeto está em fase beta e infelizmente ainda não é possível cadastrar novas contas :(");
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', logSubmit);
